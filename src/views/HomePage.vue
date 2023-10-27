92aa7fef3a644483e53f7b4a2ce7f8a8f2766554
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2023-10-27 16:02:43 +0200

    Messing with the ionic template colors. We are now ready to start building the app.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,15 +7,8 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
+      <div class="container">
         <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
   </ion-page>
@@ -41,31 +34,7 @@ export default {
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-
-  color: #8c8c8c;
-
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-toolbar {
+  color: var(--ion-text-color-light);
 }
 </style>
