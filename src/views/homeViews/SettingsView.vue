f879306a8da3b8babec3e122b47047efdf2e1b29
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2023-11-11 18:24:08 +0100

    trying avatar img with cloudinary

--- src/views/homeViews/SettingsView.vue
+++ src/views/homeViews/SettingsView.vue
@@ -1,15 +1,32 @@
 <script>
-import {IonList, IonListHeader, IonLabel, IonItem} from "@ionic/vue";
+import {IonList, IonListHeader, IonLabel, IonItem, IonToggle} from "@ionic/vue";
 import {useLoginStore} from "@/stores/LoginStore.js";
+import {ref} from 'vue';
 
 export default {
   name: "SettingsView",
   components: {
-    IonList, IonListHeader, IonLabel, IonItem
+    IonList, IonListHeader, IonLabel, IonItem, IonToggle
   },
   data() {
     return {
       loginStore: useLoginStore()
+    }
+  },
+  setup() {
+    const themeToggle = ref(false);
+
+    return {
+      themeToggle,
+    }
+  },
+  methods: {
+    toggleDarkMode(event) {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      document.body.classList.toggle('dark', !prefersDark);
+      console.log("prefernce: " + prefersDark);
+      console.log(document.body.classList)
+      console.log(event);
     }
   }
 }
@@ -24,7 +41,8 @@ export default {
       </ion-label>
     </ion-list-header>
     <ion-item>
-      Dark mode toggle
+      <ion-toggle :checked="themeToggle" @ionChange="toggleDarkMode" justify="space-between"
+      >Dark Mode</ion-toggle>
     </ion-item>
     <ion-item>
       Other app settings.
